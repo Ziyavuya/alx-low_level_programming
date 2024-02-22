8da663496505c3d8c44f3071656eeaf4b2060b6e
Siya <141371389+Ziyavuya@users.noreply.github.com>
2024-02-22 22:01:12 +0000

    learning_to_do_memory_allocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocate pointer of memory from array
+ * @nmemb: Array
+ * @size: Size of array
+ * Return: Null if enpty or fail or pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *blk;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	blk = malloc(nmemb * size);
+	if (blk != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+		{
+			blk[i] = 0;
+		}
+		return (blk);
+	}
+	else
+		return (NULL);
+}
