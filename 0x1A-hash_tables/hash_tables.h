9d87070b6ecc07528a921f8e7ce35d650630d45e
Ziyavuya <siyatal@gmail.com>
2024-04-15 05:24:49 -0700

    my_hash_table

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,14 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+/* Function prototypes */
+Node* createNode(int key, int value);
+hash_table_t* hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
+#endif /* HASH_TABLE_H */
